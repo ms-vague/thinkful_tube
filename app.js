c7968241fb82b30db28c1d63fc00f80a2a681a57
ms_vague <haha.face@gmail.com>
2017-01-29 12:58:03 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var tubeyURL = 'https://www.googleapis.com/youtube/v3/search';
+
+function getData(search){
+	var parameters = {
+		part: 'snippet',
+		key: 'AIzaSyDQONwNeCXcrqpL7aRJWwy3bbk5N4g3J_s',
+		q: searchTerm
+	}
+}
