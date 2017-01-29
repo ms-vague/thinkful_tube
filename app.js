e9aa2f1b6987244de0af8cebf0f5d9778fe046f3
ms_vague <haha.face@gmail.com>
2017-01-29 13:15:36 -0600

    fixed HTML typos, added to JS  getData function

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var tubeyURL = 'https://www.googleapis.com/youtube/v3/search';
 
+function results(data){
+	var items = data.items;
+	console.log(items[0].snippet.thumbnails.default.url);
+}
+
 function getData(search){
 	var parameters = {
 		part: 'snippet',
 		key: 'AIzaSyDQONwNeCXcrqpL7aRJWwy3bbk5N4g3J_s',
 		q: searchTerm
-	}
+	},
+	tubeyURL;
+	$.getJSON(tubeyURL, parameters, function(data) {
+		results(data);
+	});
 }
