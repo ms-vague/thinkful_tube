d8bb17902a5e77900dd412aba6519e1680d13f65
ms_vague <haha.face@gmail.com>
2017-01-31 20:38:14 -0600

    fixed JS, added some stling to CSS

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
 var tubeyURL = 'https://www.googleapis.com/youtube/v3/search';
 
-function results(data){
-	var items = data.items;
-	console.log(items[0].snippet.thumbnails.default.url);
+var watchURL = 'https://www.youtube.com/watch?v=';
+
+function getData(searchTerm, callback){
+	var query = {
+		part: 'snippet',
+		type: 'video',
+		key: 'AIzaSyCtP-cUimQUeR_OxVvPvs9pnFFuYkWbGok',
+		q: searchTerm
+	}
+	$.getJSON(tubeyURL, query, callback);
 }
 
-function getData(search){
-	var parameters = {
-		part: 'snippet',
-		key: 'AIzaSyDQONwNeCXcrqpL7aRJWwy3bbk5N4g3J_s',
-		q: searchTerm
-	},
-	tubeyURL;
-	$.getJSON(tubeyURL, parameters, function(data) {
-		results(data);
+function displayResults(data){ // get the data from youtube
+/*	var searchResults = $('.search_items');
+	searchResults.empty(); */
+	var searchResults = '';
+	if (data.items) {
+		data.items.forEach(function(item){
+			/*console.log(data.items);*/
+			var searchItemLink = watchURL + item.id.videoId;
+			var imageLink = item.snippet.thumbnails.medium.url;
+			var image = '<a href="' + searchItemLink + '"target="_blank"><img class="card-img-top" src="'+ imageLink +'" /></a>'; 
+			/* var elem = '<li class="card">' + image + '</li>'; */
+			searchResults += '<li><span>' + image + '</span></li>';
+		});
+	}
+	else {
+		searchResults += '<li>' + 'No results' + '</li>';
+	}
+	$('.search_items').html(searchResults); 
+	/* look up jquery html method. where is item/what is item? */
+}
+
+function submit(){
+	$('form').submit(function(event){
+		event.preventDefault();
+		var searchTerm = $('.js_data').val();
+		getData(searchTerm, displayResults);
 	});
 }
+
+$(function(){submit();});
